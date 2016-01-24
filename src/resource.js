e072c95e4209e1bf47d9469b1226c185402110ee
Hans Zhang <ezburg@gmail.com>
2016-01-24 16:31:33 +0100

    Added the rainbow animation

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,8 @@
 var res = {
     font_awesome: 'res/fontawesome.ttf',
     rh: 'res/rh.png',
-    rainbow: 'res/rainbow.png'
+    rainbow: 'res/rainbow.png',
+    rainbow_rect: 'res/rainbow_rect.png'
 };
 
 var g_resources = [];
--- src/sprite/slogan.js
+++ src/sprite/slogan.js
@@ -1,6 +1,10 @@
 var Slogan = cc.Node.extend({
+    RAINBOW_WIDTH: 400,
+    TIME: 2,
     rh: null,
     rainbow: null,
+    rainbowRect: null,
+    rainbowRectAgent: null,
     ctor: function () {
         this._super();
 
@@ -10,8 +14,31 @@ var Slogan = cc.Node.extend({
         this.rh.setPosition(0, -45);
         this.addChild(this.rh);
 
-        this.rainbow = new cc.Sprite(res.rainbow);
+        var rainbow = new cc.Sprite(res.rainbow);
+        this.rainbow = new cc.ClippingNode(rainbow);
+        this.rainbow.setAlphaThreshold(0.5);
         this.rainbow.setPosition(0, 45);
         this.addChild(this.rainbow);
+
+        this.rainbowRect = new cc.Sprite(res.rainbow_rect);
+        this.rainbow.addChild(this.rainbowRect);
+
+        this.rainbowRectAgent = new cc.Sprite(res.rainbow_rect);
+        this.rainbowRectAgent.setPosition(this.RAINBOW_WIDTH, 0);
+        this.rainbow.addChild(this.rainbowRectAgent);
+    },
+    onEnter: function () {
+        this._super();
+
+        this.rainbowRect.runAction(cc.sequence([
+            cc.moveBy(this.TIME, -this.RAINBOW_WIDTH, 0),
+            cc.place(this.RAINBOW_WIDTH, 0),
+            cc.moveBy(this.TIME, -this.RAINBOW_WIDTH, 0)
+        ]).repeatForever());
+
+        this.rainbowRectAgent.runAction(cc.sequence([
+            cc.moveBy(this.TIME * 2, -this.RAINBOW_WIDTH * 2, 0),
+            cc.place(this.RAINBOW_WIDTH, 0)
+        ]).repeatForever());
     }
 });
