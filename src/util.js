e353a16e7637dc02409d16ecf174e6a9021340de
Hans Zhang <ezburg@gmail.com>
2016-04-14 16:07:05 +0200

    Use helper

--- src/util.js
+++ src/util.js
@@ -1,4 +1,4 @@
-var util = {
+var Util = Cocos2dJsHelper.extend({
     // Colors
     COLOR_DARK: cc.color(34, 34, 34),
 
@@ -24,10 +24,7 @@ var util = {
     COLORS: [],
 
     // Events
-    EVENT_PLAY: 'play',
     EVENT_ROTATE: 'rotate',
-    EVENT_GAME_OVER: 'game_over',
-    EVENT_WIN_SCORE: 'win_score',
     EVENT_CHANGE_BALL: 'change_ball_color',
 
     // Collisions
@@ -41,28 +38,13 @@ var util = {
     COLLISION_EARTH: 200,
     COLLISION_METEORITE: 1000,
 
-    // config
-    CONFIG_DEBUG: 'debug',
-    CONFIG_BEST_SCORE: 'best_score',
-    configDefaults: {
-        best_score : {
-            invalid: null,
-            default: 0
-        }
-    },
-
     // Constants
     LAYER_MOVE_TIME: 0.5,
 
     // Others
-    center: null,
-    space: null,
     ballColor: null,
-    score: 0,
-    configPath: 'config.json',
     init: function () {
-        var size = cc.winSize;
-        this.center = cc.p(size.width * 0.5, size.height * 0.5);
+        this._super();
 
         this.COLORS = [
             util.COLOR_YELLOW,
@@ -81,101 +63,7 @@ var util = {
             cc.color.GREEN,
             cc.color.MAGENTA
         ];
-
-        _.forEach(this.configDefaults, function (v, k) {
-            var cv = util.config(k);
-            if (cv === v.invalid) {
-                util.config(k, v.default);
-            }
-        });
-    },
-    label: function (text, size) {
-        return new cc.LabelTTF(text, 'Arial', size || 40);
-    },
-    animate: function (prefix, count, interval) {
-        var frames = [];
-        for (var i = 1; i <= count; i++) {
-            var frame = cc.spriteFrameCache.getSpriteFrame(prefix + i + '.png');
-            frames.push(frame);
-        }
-        var ani = new cc.Animation(frames, interval);
-        return cc.animate(ani);
-    },
-    addDebugNode: function () {
-        this._debugNode = new cc.PhysicsDebugNode(util.space);
-        this._debugNode.visible = true;
-        this.addChild(this._debugNode, 9999);
-    },
-    logColors: function (colors) {
-        var result = [];
-        _.forEach(colors, function (v, k) {
-            result.push(cc.colorToHex(v));
-        });
-        cc.log(result);
-
-        return result;
-    },
-    cpVerts: function (verts) {
-        var result = [];
-        _.forEach(verts, function (v, k) {
-            result.push(v.x);
-            result.push(v.y);
-        });
-
-        return result;
-    },
-    $v2p: function (v) {
-        return cc.p(
-            v.elements[0],
-            v.elements[1]
-        );
-    },
-    p2$v: function (p) {
-        return $V([
-            p.x,
-            p.y
-        ]);
-    },
-    rotate$v2ps: function (vs, degree, origin) {
-        var result = [];
-        _.forEach(vs, function (v, k) {
-            result.push(util.$v2p(v.rotate(degree, origin)));
-        });
-        return result;
-    },
-    shadowLabel: function (text, size) {
-        var label = new cc.LabelTTF(text, null, size);
-        label.setFontFillColor(cc.color.WHITE);
-
-        var offset = cc.size(5, -5);
-        if (cc.sys.isNative) {
-            offset = cc.size(2, -2);
-        }
-        label.enableShadow(cc.color.BLACK, offset, 10);
-
-        return label;
-    },
-    config: function (k, v) {
-        if (cc.sys.isNative) {
-            var dir = jsb.fileUtils.getWritablePath();
-            var path = dir + '/' + this.configPath;
-            var jsonString = '{}';
-            if (jsb.fileUtils.isFileExist(path)) {
-                jsonString = jsb.fileUtils.getStringFromFile(path);
-            }
-            var config = JSON.parse(jsonString);
-            if (v === undefined) {
-                return config[k];
-            }
-            config[k] = v;
-            jsb.fileUtils.writeStringToFile(JSON.stringify(config), path);
-        } else {
-            if (v === undefined) {
-                v = localStorage.getItem(k);
-                return JSON.parse(v);
-            }
-            v = JSON.stringify(v);
-            localStorage.setItem(k, v);
-        }
     }
-};
+});
+
+var util = new Util();
